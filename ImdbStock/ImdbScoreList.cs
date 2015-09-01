2989fe64598aa357816e6bf91ffbcd3646158704
Luke Zhang <lukezhang213@gmail.com>
2015-08-31 21:08:04 -0400

    need to cut data off to the date of companies IPO

--- ImdbStock/ImdbScoreList.cs
+++ ImdbStock/ImdbScoreList.cs
@@ -11,9 +11,11 @@ namespace ImdbStock
 {
     class ImdbScoreList
     {
+        public const int DAYS = 5;
         struct Movie
         {
             public string id;
+            public string title;
             public DateTime releaseDate;
             public float score;
         }
@@ -41,23 +43,36 @@ namespace ImdbStock
         }
 
         private readonly string[] companyIds = { "0040938", "0026840", "0005073", "0008970", "0173285", "0000756"};
-        private readonly Tuple<string, string, string>[] companies = 
+        private readonly Tuple<string, string, string, DateTime>[] companies = 
         {
-            Tuple.Create<string, string, string>("DreamWorks", "0040938", "DWA"),
-            Tuple.Create<string, string, string>("Warner Bros", "0026840", "TWX"),
-            Tuple.Create<string, string, string>("Universal Pictures", "0005073", "CMCSA"),
-            Tuple.Create<string, string, string>("Walt Disney Pictures", "0008970", "DIS"),
-            Tuple.Create<string, string, string>("Lionsgate", "0173285", "LGF"),
-            Tuple.Create<string, string, string>("Twentieth Century Fox Film Corporation", "0000756", "FOX")
+            Tuple.Create<string, string, string, DateTime>("DreamWorks", "0040938", "DWA", new DateTime(2004, 10, 28)),
+            Tuple.Create<string, string, string, DateTime>("Warner Bros", "0026840", "TWX", new DateTime(1992, 3, 19)),
+            Tuple.Create<string, string, string, DateTime>("Universal Pictures", "0005073", "CMCSA", new DateTime(1988, 7, 7)),
+            Tuple.Create<string, string, string, DateTime>("Walt Disney Pictures", "0008970", "DIS", new DateTime(1962, 1, 2)),
+            Tuple.Create<string, string, string, DateTime>("Lionsgate", "0173285", "LGF", new DateTime(1998, 11, 17)),
+            Tuple.Create<string, string, string, DateTime>("Twentieth Century Fox Film Corporation", "0000756", "FOX", new DateTime(1987, 12, 30))
         };
         
 
         public ImdbScoreList()
         {
-            string rawHTML = RequestImdbHTML(companyIds[1]);
+            DateTime now = DateTime.Now;
+            string rawHTML = RequestImdbHTML(companyIds[0]);
             List<string> movieIds = ParseHTML(rawHTML);
-            Movie movie = RequestMovieXML(movieIds[0]);
-            YahooFinance finance = new YahooFinance();
+            foreach (string movieId in movieIds) {
+                Movie movie = RequestMovieXML(movieId);
+                if (movie.releaseDate.CompareTo(companies[0].Item4) <= 0) {
+                    break;
+                }
+                //Console.WriteLine(movie.releaseDate);
+                if (movie.releaseDate.CompareTo(now) < 0) {
+                    YahooFinance finance = new YahooFinance(companies[0].Item3);
+                    Console.WriteLine(movie.title + "\t" + movie.score);
+                    finance.RequestFinanceQuotesCSV(movie.releaseDate, new TimeSpan(DAYS, 0, 0, 0));
+                }
+                
+            }
+            
         }
 
 
@@ -108,25 +123,40 @@ namespace ImdbStock
             {
                 DateTime releasedDate = new DateTime();
                 xmlReader.ReadToFollowing("movie");
+                xmlReader.MoveToAttribute("title");
+                movie.title = xmlReader.Value;
                 xmlReader.MoveToAttribute("year");
-                int year = Int32.Parse(xmlReader.Value);
-                xmlReader.MoveToAttribute("released");
-                int month = 0;
-                int day = 0;
-                if (xmlReader.Value.Equals("N/A"))
-                {
-                    day = 0;
-                    month = 0;
+                //cleaning year string
+                string yearString = xmlReader.Value;
+                if (yearString.Length > 4) {
+                    yearString = yearString.Substring(0, 4);
+                }
+                int year = Int32.Parse(yearString);
+                //make sure there is attribute 'released'
+                if (xmlReader.GetAttribute("released") != "N/A") {
+                    xmlReader.MoveToAttribute("released");
+                    int month = 0;
+                    int day = 0;
+                    if (xmlReader.Value.Equals("N/A")) {
+                        day = 0;
+                        month = 0;
 
+                    }
+                    else {
+                        string[] date = (xmlReader.Value).Split(' ');
+                        day = Int32.Parse(date[0]);
+                        month = months[date[1]];
+                    }
+                    releasedDate = new DateTime(year, month, day);
+                    movie.releaseDate = releasedDate;
                 }
-                else
-                {
-                    string[] date = (xmlReader.Value).Split(' ');
-                    day = Int32.Parse(date[0]);
-                    month = months[date[1]];
+                //if no release date is set yet, use today's (days ago) date and the provided year
+                else {
+                    DateTime durationAgo = DateTime.Now.Subtract(new TimeSpan(DAYS, 0, 0, 0));
+                    Console.WriteLine(durationAgo);
+                    movie.releaseDate = new DateTime(year, durationAgo.Month, durationAgo.Day);
                 }
-                releasedDate = new DateTime(year, month, day);
-                movie.releaseDate = releasedDate;
+                
                 xmlReader.MoveToAttribute("imdbRating");
                 movie.score = (xmlReader.Value.Equals("N/A")) ? 0f : Single.Parse(xmlReader.Value);
             }
--- ImdbStock/YahooFinance.cs
+++ ImdbStock/YahooFinance.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +10,50 @@ namespace ImdbStock
 {
     class YahooFinance
     {
-        public YahooFinance()
-        {
+        private string companyId;
 
+
+        public YahooFinance(string companyId)
+        {
+            this.companyId = companyId;
         }
 
+        public string RequestFinanceQuotesCSV(DateTime time, TimeSpan duration)
+        {
+            //First parse the date time
+            int startMonth = time.Month - 1;
+            int startDay = time.Day;
+            int startYear = time.Year;
 
+            DateTime endDate = time.Add(duration);
+            int endMonth = endDate.Month - 1;
+            int endDay = endDate.Day;
+            int endYear = endDate.Year;
+
+            //Console.WriteLine(companyId + "\t" + startDay + "\t" + startMonth + "\t" + startYear);
+            //Console.WriteLine(companyId + "\t" + endDay + "\t" + endMonth + "\t" + endYear);
+
+            // Create a request for the URL. 
+            WebRequest request = WebRequest.Create(
+              "http://ichart.finance.yahoo.com/table.csv?s=" + companyId + "&a=" + startMonth + "&b=" + startDay + "&c=" + startYear + "&d=" + endMonth + "&e=" + endDay + "&f=" + endYear + "&g=d&ignore=.csv");
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.
+            reader.Close();
+            response.Close();
+            //Return the raw content
+            return responseFromServer;
+        }
     }
 }
